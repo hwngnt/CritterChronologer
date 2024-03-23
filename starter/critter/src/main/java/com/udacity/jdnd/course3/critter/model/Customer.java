57467eda62b4ec5a0e4e801388940aebcca3110a
Nguyễn Thành Hưng <hwngnt@hwngnt-mac.local>
2024-03-23 22:47:29 +0700

    commit model

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/model/Customer.java
@@ -0,0 +1,75 @@
+package com.udacity.jdnd.course3.critter.model;
+
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Nationalized
+    private String name; //support national characters
+    private String phoneNumber;
+    private String notes;
+    @OneToMany(
+            mappedBy = "owner",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Pet> pets = new ArrayList<>();
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String name, String phoneNumber, String notes, List<Pet> pets) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.notes = notes;
+        this.pets = pets;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+}
---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/model/Pet.java
@@ -0,0 +1,80 @@
+package com.udacity.jdnd.course3.critter.model;
+
+import com.udacity.jdnd.course3.critter.pet.PetType;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ElementCollection
+    private PetType type;
+    private String name;
+    private LocalDate birthDate;
+    private String notes;
+    @ManyToOne
+    private Customer owner;
+
+    public Pet() {
+    }
+
+    public Pet(Long id, PetType type, String name, LocalDate birthDate, String notes, Customer owner) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.notes = notes;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+}
